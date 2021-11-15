336d8e6bf07d6a710fdb1eb7990b71479d7f3828
Greta Aksamitauskaite <atataite@gmail.com>
2021-11-15 18:59:02 +0200

    4: API failas

---
+++ js/API.js
@@ -0,0 +1,26 @@
+const baseURL= 'http://localhost:3000';
+
+class API {
+    static fetchApartments= (success,failure) => {
+        fetch(`${baseURL}/apartments`)
+        .then (res => res.json())
+        .then(success)
+        .catch(failure)
+    }
+    static deleteApartments = (id,success,failure) => {
+        fetch(`${baseURL}/apartments/${id}`),{method:'DELETE'}
+        .then(res=>res.ok? success(): failure(res.statusText))
+        .catch(failure)
+    }
+}
+
+//API.fetchApartments(
+//    console.log,
+//    console.error
+//)
+
+//API.deleteApartments(
+//    "3",
+//    () => console.log('Ištrinta sėkmingai'),
+//    console.error
+//)//
